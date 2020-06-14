92d64fb34fbb198ac31b1bb85c71b0f292c4fb28
Jelhill <talk2jelili2000@gmail.com>
2020-06-14 21:23:54 +0100

    Updated Header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="headerPurpleDiv">
+                <div className="headerIconDiv">
+                    <div className="leftIconDiv">
+                        <h3>Logo</h3>
+                        <span><Link><i className="fa fa-home"></i></Link></span>
+                    </div>
+                    <div className="rightIconDiv"> 
+                        <span className="font15 mgTop10">Welcome Jelhill</span>                             
+                        <Link><span><i className="fa fa-whatsapp"></i></span></Link>
+                        <Link><span><i className="fa fa-bars"></i></span></Link>
+                        <Link><span><i className="fa fa-phone-alt"></i></span></Link>
+                        <span className="font15 mgTop10">+2348025992525</span>                            
+                    </div>
+                </div>
+                <div className="innerPurpleDiv">
+                    <div className="inputButtonDiv">
+                        <input type="text" className="inputSpecial" placeholder="Search Room"/>
+                        <button className="buttonSpecial">Today: June 8, 2020</button>
+                        <button className="buttonSpecial2">Today: June 8, 2020</button>
+                        <i class="fa fa-calendar form-control-feedback"></i>
+                    </div>                        
+                </div>
+            </div> 
+        )
+    }
+}
