e2e8f2a452b39251d8541eac4f3217c566b16553
waterman001 <afeksolamide.com>
2020-06-19 14:15:58 +0100

    server in progress

--- Server/routes/router.js
+++ Server/routes/router.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const router = express.Router()
+const db = require('../config/db')
+
 
 
 router.get("/home", (req, res) => {
     res.json("Welcome to our App")
 }) 
+
+router.post("/signUp", async (req, res, next) => {
+    try{ 
+        const {surname,firstName,otherName,email,username,password,gender,imageUrl} = req.body
+        const result = await db.query("INSERT into users (surname,firstName,otherName,email,username,password,gender,imageUrl) VALUES($1,$2,$3,$4,$5,$6,$7,$8)",[
+            surname,firstName,otherName,email,username,password,gender,imageUrl
+        ]);
+        res.json(result.rows[0])
+    }
+    catch(e){
+next(e)
+    }
+});
+
+
 module.exports = router
