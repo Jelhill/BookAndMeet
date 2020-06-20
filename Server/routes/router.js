60534fcaa2478ffc5b72be2619c0d4773a1405ca
waterman001 <afeksolamide.com>
2020-06-20 11:34:36 +0100

    for server

--- Server/routes/router.js
+++ Server/routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const db = require('../config/db')
+require("dotenv").config()
+
 
 
 
@@ -11,7 +13,7 @@ router.get("/home", (req, res) => {
 router.post("/signUp", async (req, res, next) => {
     try{ 
         const {surname,firstName,otherName,email,username,password,gender,imageUrl} = req.body
-        const result = await db.query("INSERT into users (surname,firstName,otherName,email,username,password,gender,imageUrl) VALUES($1,$2,$3,$4,$5,$6,$7,$8)",[
+        const result = await db.query("INSERT into users (surname,firstName,otherName,email,username,password,gender,imageUrl) VALUES($1,$2,$3,$4,$5,$6,$7,$8) returning *",[
             surname,firstName,otherName,email,username,password,gender,imageUrl
         ]);
         res.json(result.rows[0])
