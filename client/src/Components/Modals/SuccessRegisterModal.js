fe3a0c13765185fc4b288a3c05be88edb6a8c548
tobasanmi <tobasanmi@gmail.com>
2020-08-15 18:09:02 +0100

    succes reg Modal

--- client/src/Components/Modals/SuccessRegisterModal.js
+++ client/src/Components/Modals/SuccessRegisterModal.js
@@ -1,76 +1,51 @@
-import React, {Component} from 'react';
-import { Link, withRouter } from "react-router-dom"
-import { connect } from 'react-redux';
-import { showSignup, saveInitialUserDetails, getSignupInputs } from "../../Actions/userActions"
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+// import showRegSuccessfull from '../../Reducers/userReducer'
+import {withRouter} from 'react-router-dom'
 
-class CompleteSignUp extends Component{
 
-    onClose = (e) => {
-      e.preventDefault()
-      this.props.showSignup(false)
-      window.location = "/"
-    } 
-
-    getUserInput = (e) => {
-      this.props.getSignupInputs({[e.target.name]: e.target.value})
-    }
-
-    handleSignup = async (e) => {
-      e.preventDefault()
-      fetch("http://localhost:3001/signUp", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(this.props.signUpFormDetails)
-      })
-      .then(response => response.json())
-      .then((jsonResponse) => {
-        console.log(jsonResponse)
-        
-      })
-      .catch((err) => console.log(err))
-    }
-
-    render(){
-        if(!this.props.show3){
-            return null;
-        }
-        return(
-            <div className="modalDiv2">
-              <div className="signupmodal">
-                  <div className="leftModal">
-                      <h1 className="boardRoom">BoardRoom</h1>
+class SuccessRegisterModal extends Component {
+  // onClose = () => {
+  //   // e.preventDefault()
+  //   this.props.showSuccessfullRegModal(false)  
+  // }
+  render() {
+  //   if(!this.props.showSuccessfullRegModal){
+  //     return null;
+  // }
+    return (
+      <div className="modalDiv">
+        <div className="signupmodal">
+          <div className="leftModal">
+            <h1 className="boardRoom">BoardRoom</h1>
+          </div>
+          <div className="rightModal">
+          <button className="closeWrapper"><span className="close" onClick={this.onClose} >&times;</span></button>
+                <div>
+                  <div className="successContentWrapper">
+                  <h1 className="thankyou">Thank You </h1>
+                  <p className="paragraph1">Thank you for signing up with BoardRoom we hope you enjoy your time with us </p>
+                  <p className="paragraph2">Check your account and update your profile</p>
                   </div>
-                  <div className="rightModal">
-                      <div className="signupheader">
-                          <button className="closeWrapper"><span className="close" onClick={this.onClose} >&times;</span></button>
-                          <h3 className="welcome">Complete registration</h3>
-                      </div>
-                  
-                      <div className="modalContent">
-                        <button className="form-control signupsubmit" onClick={this.handleSignup}>Complete</button>
-                      </div>                   
-                      <p className="loginparagraph">Already Registered? <Link to="#">Sign in</Link></p>
+                  <div>
+                    <button className="contdButton">Continue</button>
                   </div>
-                      </div>
-                  </div>
-        )
-    }
+                  <div><button className="myAccButton">My Account</button></div>
+                </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
 const mapStateToProps = (state) => {
   const { userReducer } = state
+  console.log('logging', userReducer.showSuccessfullRegModal);
+  // const now = new Date()
+  //     console.log("Login State", now.getTime(), userReducer.loggedInUserInfo)
   return {
-    signUpFormDetails: userReducer.signUpFormDetails,
-    showSignUp: userReducer.showSignUp
+    showSuccessfullRegModal: userReducer.showSuccessfullRegModal
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    showSignup: (values) =>  dispatch(showSignup(values)),
-    getSignupInputs: (values) => dispatch(getSignupInputs(values)),
-    saveInitialUserDetails: (values) => dispatch(saveInitialUserDetails(values))
-  }
-}
-
-  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CompleteSignUp));
+export default withRouter(connect(mapStateToProps)(SuccessRegisterModal));
