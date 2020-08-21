3d64ee5949563af2358e5f3b420596d9d7f5d2cd
Jelhill <talk2jelili2000@gmail.com>
2020-08-21 14:18:46 +0100

    Search filter update

--- client/src/Components/HomePage/SearchFilter.js
+++ client/src/Components/HomePage/SearchFilter.js
@@ -1,30 +1,102 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { updateStateWithSearchInput, updateStateWithFilteredRoom } from  "../../Actions/roomActions"
 
-export default class seachFilter extends Component {
+
+
+class seachFilter extends Component {
+    
+    getSearchInput = (e) => {
+        this.props.updateStateWithSearchInput({[e.target.name]: e.target.value})
+    }
+    
+    searchForRooms = (e) => {
+        e.preventDefault()                
+        const { roomType, roomCapacity, dateSearch, timeSearch } = this.props
+        let minimum = 0, maximum = 0
+        switch(roomCapacity) {
+            case "oneToTen":
+                minimum = 1
+                maximum = 10
+                break;
+            case "elevenToFifty":
+                minimum = 11
+                maximum = 50
+                break;
+            case "fiftyOneToOneHundred":
+                minimum = 51
+                maximum = 100
+                break;
+            case "hundredToTwoHundred":
+                minimum = 101
+                maximum = 200
+                break;
+            case "aboveTwoHundred":
+                minimum = 201
+                maximum = 5000
+                break;
+            default: 
+                minimum = 0;
+                maximum = 0;
+        }
+
+        const filteredRoom = this.props.rooms.filter(room => {
+            console.log("MMMR", minimum, maximum, roomCapacity)
+            if(room.type === roomType && (room.capacity >= minimum && room.capacity <= maximum) ){
+                return true
+            }
+            return false
+        })
+        this.props.updateStateWithFilteredRoom(filteredRoom)
+    }
+
     render() {
         return (
             <div className="searchAreaDiv">
-                <select name="" defaultValue={"Type"} >
-                    <option disabled selected hidden>Type</option>
-                    <option value="boardRoom">Board Room</option>
-                    <option value="privateRoom">Private Room</option>
-                    <option value="conferenceRoom">Conference</option>
-                    <option value="auditorium">Auditorium</option>
+                <select name="roomType" defaultValue={"Type"} onChange={this.getSearchInput}>
+                    <option disabled hidden>Type</option>
+                    <option value="Board Room">Board Room</option>
+                    <option value="Private Room">Private Room</option>
+                    <option value="Conference Room">Conference</option>
+                    <option value="Auditorium">Auditorium</option>
                 </select>
 
-                <select name="" defaultValue={"Capacity"} >
-                    <option disabled selected hidden>Capacity</option>
+                <select name="capacity" defaultValue={"Capacity"} onChange={this.getSearchInput}>
+                    <option disabled hidden>Capacity</option>
                     <option value="oneToTen">1 - 10 seats</option>
-                    <option value="elevenToTwenty">11 - 50 seats</option>
+                    <option value="elevenToFifty">11 - 50 seats</option>
                     <option value="fiftyOneToOneHundred">51 - 100 seats</option>
                     <option value="hundredToTwoHundred">101 - 200 seats</option>
                     <option value="aboveTwoHundred">Above 200 seats</option>
                 </select>
-                <input type="date"/>
-                <input type="time"/>
-                <button>Search</button>
+                <input type="date" name="dateSearch" onChange={this.getSearchInput}/>
+                <input type="time" name="timeSearch" onChange={this.getSearchInput}/>
+                <button onClick={this.searchForRooms}>Search</button>
             </div>
-
         )
     }
 }
+
+
+const mapStateToProps = (state) => {
+    const { roomReducer } = state
+    console.log("Rooms", roomReducer.rooms)
+    console.log("Filtered ROoms", roomReducer.filteredRoom)
+
+    return {
+        rooms: roomReducer.rooms,
+        roomType: roomReducer.searchInput.roomType,
+        roomCapacity: roomReducer.searchInput.capacity,
+        dateSearch: roomReducer.searchInput.dateSearch,
+        timeSearch: roomReducer.searchInput.timeSearch,  
+        filteredRoom: roomReducer.filteredRoom  
+    }   
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateStateWithSearchInput: (values) => dispatch(updateStateWithSearchInput(values)),
+        updateStateWithFilteredRoom: (values) => dispatch(updateStateWithFilteredRoom(values))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(seachFilter);
