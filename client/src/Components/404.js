f46418cbc6b49480dbb8fcdfe8af5fbf09846db0
waterman001 <afeksolamide.com>
2020-08-22 22:37:56 +0100

    404 page

--- client/src/Components/404.js
+++ client/src/Components/404.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Error404() {
     return (
-        <div>
-            <h1>404 Error!!!</h1>
-            <h2>Access Denied</h2>
+        <div className="Error">
+            <div className="Big404">
+                <p>404</p>
+            </div>
+            <div className="Ooops">
+            <small>Ooops!!</small>
+            </div>
+            <div className="PageExist">
+            <span>THAT PAGE DOESN'T EXIST OR IS UNAVAILABLE</span>
+            </div>
+            <Link to="/home"><button className="Button404">Go Back to Home</button></Link>
         </div>
     )
 }
