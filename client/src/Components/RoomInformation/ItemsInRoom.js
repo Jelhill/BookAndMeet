b0afdf6a9307eef732551bb0c502bd5b564bd831
Jelhill <talk2jelili2000@gmail.com>
2020-08-23 10:05:31 +0100

    update

--- client/src/Components/RoomInformation/ItemsInRoom.js
+++ client/src/Components/RoomInformation/ItemsInRoom.js
@@ -1,40 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import ImageAirConditioner from "../../Images/air-conditioner.png";
+import ImageProjector from "../../Images/projector.png";
+import ImageWaterCooler from "../../Images/water-cooler.png";
+import ImagePresentation from "../../Images/presentation.png";
+import ImageFriend from "../../Images/friend.png";
+
 
 function ItemsInRoom (props){
     const {hasAirCondition, hasProjector, hasWaterDispenser, hasWhiteBoard, capacity } = props
         return (
             <div className="roomInfoDiv">
                 <div className="roomInfoSpanDiv">
-                    <span><i className="fa fa-chair"></i></span> 
+
+                    <span> <img src={ImageFriend} alt="Room 5" /></span>
                     <div className="flexItemsInRoom">
                         <span>Capacity</span>
                         <span>{capacity} Seats</span>
                     </div>
                 </div>
                 <div className="roomInfoSpanDiv">
-                    <span><i className="fa fa-chair"></i></span> 
+                   <span> <img src={ImageAirConditioner} alt="Room 5" /></span>
                     <div className="flexItemsInRoom">
                         <span>Air Condition</span>
                         <span>{hasAirCondition ? "Yes" : "No"}</span>
                     </div>
                 </div>
                 <div className="roomInfoSpanDiv">
-                    <span><i className="fa fa-chair"></i></span> 
+                    <span> <img src={ImageProjector} alt="Room 5" /></span>
                     <div className="flexItemsInRoom">
                         <span>Projector</span>
                         <span>{hasProjector ? "Yes" : "No"}</span>
                     </div>
                 </div>
                 <div className="roomInfoSpanDiv">
-                    <span><i className="fa fa-chair"></i></span> 
+                    <span> <img src={ImageWaterCooler} alt="Room 5" /></span>
                     <div className="flexItemsInRoom">
                         <span>Water Dispenser</span>
                         <span>{hasWaterDispenser ? "Yes" : "No"}</span>
                     </div>
                 </div>
-                <div className="roomInfoSpanDiv">
-                    <span><i className="fa fa-chair"></i></span> 
+                    <div className="roomInfoSpanDiv">
+                    <span> <img src={ImagePresentation} alt="Room 5" /></span>
+
                     <div className="flexItemsInRoom">
                         <span>White Board</span>
                         <span>{hasWhiteBoard ? "Yes" : "No"}</span>
